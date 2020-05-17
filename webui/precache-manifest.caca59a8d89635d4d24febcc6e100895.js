79ea706c309e2d9f2b8b4800cd67ab89caa61109
hzwy23 <hzwy23@163.com>
2020-05-17 21:47:31 +0800

    update

---
+++ webui/precache-manifest.caca59a8d89635d4d24febcc6e100895.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = [
+  {
+    "revision": "2dbc4297a5478d106ecd",
+    "url": "/ui/vendors.3d6c8b85.chunk.css"
+  },
+  {
+    "revision": "2dbc4297a5478d106ecd",
+    "url": "/ui/vendors.c2ee683f.async.js"
+  },
+  {
+    "revision": "e81d9f02e451dedd755a",
+    "url": "/ui/layouts__BasicLayout.2fab1ec2.chunk.css"
+  },
+  {
+    "revision": "e81d9f02e451dedd755a",
+    "url": "/ui/layouts__BasicLayout.d2e8a895.async.js"
+  },
+  {
+    "revision": "fd423d91d075813c2b35",
+    "url": "/ui/layouts__UserLayout.ef57b16c.chunk.css"
+  },
+  {
+    "revision": "fd423d91d075813c2b35",
+    "url": "/ui/layouts__UserLayout.7b0a47d4.async.js"
+  },
+  {
+    "revision": "d305747a2d9b7beee77a",
+    "url": "/ui/p__404.207204da.chunk.css"
+  },
+  {
+    "revision": "d305747a2d9b7beee77a",
+    "url": "/ui/p__404.17ebdf02.async.js"
+  },
+  {
+    "revision": "a7a8c4d666c044cc6172",
+    "url": "/ui/p__Device__Group__AddGroup.af17f393.async.js"
+  },
+  {
+    "revision": "018b9b74da5b049ed606",
+    "url": "/ui/p__Device__Group__DeviceList.b9b7c526.async.js"
+  },
+  {
+    "revision": "5995c7d68ac3d96fc712",
+    "url": "/ui/p__Device__Group__EditGroup.2b1f9106.async.js"
+  },
+  {
+    "revision": "de779a21584ddf3c661c",
+    "url": "/ui/p__Device__Group__Index.8d7300c3.async.js"
+  },
+  {
+    "revision": "278265589bfc4fc2c443",
+    "url": "/ui/p__Device__Install__Add.53afed77.async.js"
+  },
+  {
+    "revision": "2fc4557e0f6dc8de02a7",
+    "url": "/ui/p__Device__Install__Edit.dcab0b83.async.js"
+  },
+  {
+    "revision": "986368c428ed75b4380f",
+    "url": "/ui/p__Device__Install__Index.ae1a395d.chunk.css"
+  },
+  {
+    "revision": "986368c428ed75b4380f",
+    "url": "/ui/p__Device__Install__Index.b6d394b2.async.js"
+  },
+  {
+    "revision": "72edfd64b0f1b9c14bed",
+    "url": "/ui/p__Device__Install__View.3dcc57ae.async.js"
+  },
+  {
+    "revision": "1b94d10f43202322e081",
+    "url": "/ui/p__Device__Manage__EditDevice.6a2a4eeb.async.js"
+  },
+  {
+    "revision": "1989c86975ba2dcc5b43",
+    "url": "/ui/p__Device__Manage__Index.837304f1.chunk.css"
+  },
+  {
+    "revision": "1989c86975ba2dcc5b43",
+    "url": "/ui/p__Device__Manage__Index.41a7ad58.async.js"
+  },
+  {
+    "revision": "ed1b16f4d3b091254cdc",
+    "url": "/ui/p__Device__Scan__AddDevice.128b7c9b.async.js"
+  },
+  {
+    "revision": "ed12c60987987cb48a18",
+    "url": "/ui/p__Device__Scan__EditDeviceNetwork.92f4cab5.async.js"
+  },
+  {
+    "revision": "dbed96884c087469bfdd",
+    "url": "/ui/p__Device__Scan__Index.a7b966f7.chunk.css"
+  },
+  {
+    "revision": "dbed96884c087469bfdd",
+    "url": "/ui/p__Device__Scan__Index.076d8175.async.js"
+  },
+  {
+    "revision": "6ffc7e3ea4c2b617ba75",
+    "url": "/ui/p__Event__Lamp__Index.10f92f8f.chunk.css"
+  },
+  {
+    "revision": "6ffc7e3ea4c2b617ba75",
+    "url": "/ui/p__Event__Lamp__Index.03a85dcd.async.js"
+  },
+  {
+    "revision": "4f95d5d4332ae841c5f5",
+    "url": "/ui/p__Event__Offline__Index.797181e9.chunk.css"
+  },
+  {
+    "revision": "4f95d5d4332ae841c5f5",
+    "url": "/ui/p__Event__Offline__Index.18518126.async.js"
+  },
+  {
+    "revision": "e03876c682c606f748a6",
+    "url": "/ui/p__Event__Temperature__Index.8505c0c1.chunk.css"
+  },
+  {
+    "revision": "e03876c682c606f748a6",
+    "url": "/ui/p__Event__Temperature__Index.bb0535ca.async.js"
+  },
+  {
+    "revision": "8b83bfe780a9551385fa",
+    "url": "/ui/p__System__Account__Index.9c9fb0a8.async.js"
+  },
+  {
+    "revision": "8e076e516b3c9c6e65c1",
+    "url": "/ui/p__System__Config__Index.a9bcc793.async.js"
+  },
+  {
+    "revision": "6ceb638826c00824a170",
+    "url": "/ui/p__System__Grant__Index.7aea998c.async.js"
+  },
+  {
+    "revision": "43e373a3ce4a30069eee",
+    "url": "/ui/p__System__Logger__Device__Index.6b3a8223.chunk.css"
+  },
+  {
+    "revision": "43e373a3ce4a30069eee",
+    "url": "/ui/p__System__Logger__Device__Index.9122644c.async.js"
+  },
+  {
+    "revision": "ed4a674fabcbe7056245",
+    "url": "/ui/p__System__Logger__Index.28dc16b2.chunk.css"
+  },
+  {
+    "revision": "ed4a674fabcbe7056245",
+    "url": "/ui/p__System__Logger__Index.9e9720e6.async.js"
+  },
+  {
+    "revision": "5583040b34897a23a523",
+    "url": "/ui/p__System__Role__Index.6216ef80.async.js"
+  },
+  {
+    "revision": "894a3f0f3b406afcee0b",
+    "url": "/ui/p__User__Login.01309caf.chunk.css"
+  },
+  {
+    "revision": "894a3f0f3b406afcee0b",
+    "url": "/ui/p__User__Login.fff82dcd.async.js"
+  },
+  {
+    "revision": "6097b47f30c265f7620b",
+    "url": "/ui/p__User__Password.4ee77656.async.js"
+  },
+  {
+    "revision": "ffc04490635911c302db",
+    "url": "/ui/p__User__Profiles__Index.87bb1cd4.async.js"
+  },
+  {
+    "revision": "6804ed90b483c6562197",
+    "url": "/ui/p__Welcome.4b08a5c0.chunk.css"
+  },
+  {
+    "revision": "6804ed90b483c6562197",
+    "url": "/ui/p__Welcome.f723abd6.async.js"
+  },
+  {
+    "revision": "fb777fac41123ef46957",
+    "url": "/ui/umi.65fe7970.css"
+  },
+  {
+    "revision": "fb777fac41123ef46957",
+    "url": "/ui/umi.e2303ec9.js"
+  },
+  {
+    "revision": "499769128fd73adf00b894650ebf97e4",
+    "url": "/ui/static/logo.49976912.png"
+  },
+  {
+    "revision": "a9eb210e9c42e6a079be1dce733fee86",
+    "url": "/ui/index.html"
+  },
+  {
+    "revision": "4c0f1c8b54384f15e32bea691ea91acd",
+    "url": "/ui/bad.png"
+  },
+  {
+    "revision": "c23aa881b8d0e959f6c0aeb5ae7e4baa",
+    "url": "/ui/icons/icon-192x192.png"
+  },
+  {
+    "revision": "5661ce8e2472d2727e8fd0390b22ba37",
+    "url": "/ui/icons/icon-512x512.png"
+  },
+  {
+    "revision": "b285be5f5e4f6c4fea04c09783712940",
+    "url": "/ui/normal.png"
+  },
+  {
+    "revision": "694c4300be571fdd4d8b6abbe14d2e3f",
+    "url": "/ui/favicon.png"
+  },
+  {
+    "revision": "b1994b4d200f6f5de9b8c7577bcc6d14",
+    "url": "/ui/right.png"
+  },
+  {
+    "revision": "347329d0982682b42d40c8162817a940",
+    "url": "/ui/icons/icon-128x128.png"
+  },
+  {
+    "revision": "e147bdebf32ef9cb7ce137ed0ed5d3a6",
+    "url": "/ui/avatar.png"
+  },
+  {
+    "revision": "194577a7e20bdcc7afbb718f502c134c",
+    "url": "/ui/.DS_Store"
+  },
+  {
+    "revision": "32ebaab97324e0ef4b91a6136dd3ce44",
+    "url": "/ui/manifest.json"
+  },
+  {
+    "revision": "b19327e3e83ce06113ae14c0d0497903",
+    "url": "/ui/pwacompat.min.js"
+  }
+];
